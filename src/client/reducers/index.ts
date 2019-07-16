c3c50d97c32dadcd2f1f12046336c9856ab46c9f
Dan Kreiger <dan@dankreiger.com>
2019-07-16 09:06:23 +0200

    chore(ssr): cleanup

--- src/client/reducers/index.ts
+++ src/client/reducers/index.ts
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users.reducer';
-import { UsersReducerState } from './users.reducer.types';
 
 export default combineReducers({
     users: usersReducer,
